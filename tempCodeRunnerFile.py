d3ab4f51d0677d215f89a727eba968fd27258c65
Bade om <71856738+ombade@users.noreply.github.com>
2023-08-02 12:14:08 +0530

    file renamed

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-ymongo.MongoClient( "mongodb+srv://ombade364:Password@107@cluster0.qlwkzo3.mongodb.net/?retryWrites=true&w=majority")
-                # db = client['review_scrap']
-                # reviews_col = db['review_scrap_data']
-                # reviews_col.insert_many(reviews)
