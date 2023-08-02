8d5e06c42b187921f584cc4f3375bea288ec273f
Bade om <71856738+ombade@users.noreply.github.com>
2023-08-02 19:14:44 +0530

    Update application.py

--- application.py
+++ application.py
@@ -95,7 +95,7 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
     # app.run(debug=True)
 
 # from flask import Flask, render_template, request, jsonify
